30b2c5e22fa5e2d333480e62ba7ffac11862f621
Robert Allen <robert.allen@princetoninsurance.com>
2015-09-25 15:15:32 -0400

    Added more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## Can place a matrix in the cache and call the matrix from the cache or
 ## compute a matrix
 
+##This function creates a matrix and stores it in the cache
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y){
@@ -15,7 +16,10 @@ makeCacheMatrix <- function(x = matrix()) {
          getmatrix = getmatrix)
 }
 
+##This function resturns the inverse of a matrix, checking in the cache
+##first
 cacheSolve <- function(x=matrix(), ...) {
+    ## Return a matrix that is the inverse of 'x'
     m <- x$getmatrix()
     if(!is.null(m)) {
         message("Getting cached data...")
